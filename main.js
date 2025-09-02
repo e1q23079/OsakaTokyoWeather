3d511de148021d5bef800b3f3d5d2f9c05a58a41
e1q23079 <e1q23079@oit.ac.jp>
2025-09-02 21:32:17 +0900

    時間帯により背景色を制御

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const ejs = require('ejs');
+const { time } = require('console');
 
 // リクエストオプション
 const requestOptions = {
@@ -28,25 +29,44 @@ const main = async () => {
     console.log("Running... Osaka and Tokyo Weather!");
 
     // 大阪の天気を取得
-    console.log("大阪");
+    // console.log("大阪");
     const osaka = await getData('270000');
     const osakaWeather = osaka["forecasts"][0]["image"];
+    /*
     console.log(osakaWeather["title"]);
     console.log(osakaWeather["url"]);
+    */
 
     // 東京の天気を取得
-    console.log("東京");
+    // console.log("東京");
     const tokyo = await getData('130010');
     const tokyoWeather = tokyo["forecasts"][0]["image"];
+    /*
     console.log(tokyoWeather["title"]);
     console.log(tokyoWeather["url"]);
+    */
+
+    // 時刻を取得
+    const now = new Date();
+    // 時間を取得
+    const hour = now.getHours();
+
+    // 時間帯を判定
+    let timeClass = '';
+    if (hour >= 6 && hour < 16) {
+        timeClass = 'morning';
+    } else if (hour >= 16 && hour < 19) {
+        timeClass = 'evening';
+    } else {
+        timeClass = 'night';
+    }
 
     // HTMLを生成
     const indexPage = fs.readFileSync('./index.ejs', 'utf8');
     const html = ejs.render(indexPage, {
         osaka: osakaWeather,
         tokyo: tokyoWeather,
-        timeClass: 'night'
+        timeClass: timeClass
     });
     fs.writeFileSync('./index.html', html);
     console.log("index.html generated!");
