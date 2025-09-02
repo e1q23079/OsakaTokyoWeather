6f1dddc8c4e009fce9c18b161aaeb9ca817d5643
e1q23079 <e1q23079@oit.ac.jp>
2025-09-02 15:11:19 +0900

    天気データ取得

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("Hello, Osaka and Tokyo Weather!");
+// リクエストオプション
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+};
+
+// 天気用法を取得：引数（市）
+const getData = async (city) => {
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+    };
+
+    try {
+        const response = await fetch(`https://weather.tsukumijima.net/api/forecast?city=${city}`, requestOptions);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log("Error");
+    }
+};
+
+// メイン関数
+const main = async () => {
+    console.log("Running... Osaka and Tokyo Weather!");
+    console.log("大阪");
+    const osaka = await getData('270000');
+    console.log(osaka);
+    console.log("東京");
+    const tokyo = await getData('130010');
+    console.log(tokyo);
+}
+
+
+main();
